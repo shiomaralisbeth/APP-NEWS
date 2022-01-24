c87e0b7b179a871f624336a277fe0777448bb987
Shiomara Huaracallo <shiomara.hy@gmail.com>
2022-01-24 13:15:21 -0500

    Configuracion WebView

--- app/src/main/java/com/shiomara/appnews/presentation/ui/view/DetailNoticiaActivity.kt
+++ app/src/main/java/com/shiomara/appnews/presentation/ui/view/DetailNoticiaActivity.kt
@@ -1,11 +1,14 @@
 package com.shiomara.appnews.presentation.ui.view
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -15,13 +18,14 @@ import com.shiomara.appnews.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_detalle_noticia.*
 
+
 class DetailNoticiaActivity: FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_detalle_noticia)
         val extras = intent.extras
         val url = extras?.getString(URLSTORY)
-
+        setUpWebView()
         setUI(url)
 
     }
@@ -31,6 +35,43 @@ class DetailNoticiaActivity: FragmentActivity() {
             webview.loadUrl(url)
         }
     }
+
+    private fun setUpWebView() {
+
+        val webSettings = webview.settings
+
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        webSettings.setAppCacheEnabled(true)
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webSettings.setGeolocationEnabled(true)
+
+        webview.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+                webview_progressbar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+
+                webview_progressbar.visibility = View.GONE
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+            }
+        }
+
+        webview.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                webview_progressbar.progress = newProgress
+            }
+        }
+    }
+
     companion object {
         const val URLSTORY = "URLSTORY"
     }
--- app/src/main/java/com/shiomara/appnews/presentation/ui/viewmodel/DetailNoticiaViewModel.kt
+++ app/src/main/java/com/shiomara/appnews/presentation/ui/viewmodel/DetailNoticiaViewModel.kt
@@ -1,9 +1,23 @@
 package com.shiomara.appnews.presentation.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shiomara.appnews.domain.noticias.GetNoticiasList
 import com.shiomara.appnews.domain.noticias.Noticia
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class DetailNoticiaViewModel() :
-    BaseViewModel<Any, Any>() {
+class DetailNoticiaViewModel() : ViewModel() {
+
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val  isLoading: LiveData<Boolean> get() = _isLoading
+
+    private val _urlNoticia = MutableLiveData<String>()
+    val  urlNoticia: LiveData<String> get() = _urlNoticia
+
+
 
 }
